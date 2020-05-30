8795bd0f2d447e6dc297e1dc564e72c0616586ee
angelpsch <angelpsch@users.noreply.github.com>
2020-05-30 10:33:47 -0500

    add comments and such

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,7 +6,7 @@ router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-//Send other files to index.html
+//Send other requests to index.html
 router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
