908846ec2d7aa186c5372078bee2121bfdde1eca
angelpsch <angelpsch@users.noreply.github.com>
2020-07-14 13:59:21 -0500

    changing route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,7 +7,7 @@ router.get("/notes", (req, res) => {
 });
 
 //Send other requests to index.html
-router.get("*", (req, res) => {
+router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
