d9479dca316eb83958e65df0fac8d94e864c5089
angelpsch <angelpsch@users.noreply.github.com>
2020-07-14 14:05:16 -0500

    trying to fix deploy issue

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
