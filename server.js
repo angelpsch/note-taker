c382cf113861224b3a76d89fa7dbbcbea7660f60
angelpsch <angelpsch@users.noreply.github.com>
2020-07-22 08:25:05 -0500

    -

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require("express");
-// const path = require('path');
-// const fs = require('fs'); 
-
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
-
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
