7509a1a505eb9ad0c858f672191a623f003bcb23
angelpsch <angelpsch@users.noreply.github.com>
2020-07-22 08:25:46 -0500

    -

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("./public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
+//routes
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
