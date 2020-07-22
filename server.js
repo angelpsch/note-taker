e51fe1a9679785472022ff282cc9bdf90921431e
angelpsch <angelpsch@users.noreply.github.com>
2020-07-22 08:00:40 -0500

    remove node mod folder

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require('path');
+// const path = require('path');
 // const fs = require('fs'); 
 
 const apiRoutes = require("./routes/apiRoutes");
@@ -12,8 +12,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
