f56acaeb26ca13eb333ef4f93c4d4715897088e0
angelpsch <angelpsch@users.noreply.github.com>
2020-08-12 09:31:24 -0500

    deploy fix

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const app = express();
-const PORT = process.env.PORT || 8080;
+
 
 //routes
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
-
+const app = express();
+const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
