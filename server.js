ab7562c8ebd816678b96f8872ed6327de02af5c6
angelpsch <angelpsch@users.noreply.github.com>
2020-08-12 09:29:53 -0500

    deploy fix

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-
+//routes
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,8 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", apiRoutes);
 app.use("", htmlRoutes);
 
-//routes
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+
 
 app.listen(PORT, () => console.log("App listening on PORT " + PORT));
